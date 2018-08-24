c31235e1230c1efa4f9c60ebbbacb4222e9534d2
shivaraj <sbh69840@gmail.com>
2018-08-25 04:57:04 +0530

    assignment-2-equilateral-triangle

--- triangle.c
+++ triangle.c
@@ -3,6 +3,13 @@ void input(int *rows){
 	printf("enter number of rows.");
 	scanf("%d",rows);
 }
+/*
+print_triangle receives 1 parameter:
+INPUT PARAMETERS rows: no. of rows of triangle
+OUTPUT PARAMETERS printf: prints the triangle
+RETURN PARAMETERS : doesn't return anything.
+Note: It will print a triangle with equal sides of n number of rows. 
+*/
 void print_triangle(int rows){
 	int i,white_space,j;
 	for(i=0;i<rows;i++){
