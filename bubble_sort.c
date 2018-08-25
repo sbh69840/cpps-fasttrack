fa2c6027dd271687218e6ba743e36213b24389a0
shivaraj <sbh69840@gmail.com>
2018-08-25 18:34:22 +0530

    assignment-4-bubble_sort

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 void bubble_sort(float a[],float length);
-void input_length(float *length);
+void input_length(int *length);
 void input_array(float a[],float length);
-void output(float a[]);
+void output(float a[],float length);
 void main(){
-    float length;
+    int length;
     input_length(&length);
     float a[length];
-    input_array(a[],float length);
-    bubble_sort(a[],float length);
-    output(a[]);
+    input_array(a,length);
+    bubble_sort(a,length);
+    output(a,length);
 
 }
-void input_length(float *length){
+void input_length(int *length){
     printf("Enter length of array");
-    scanf("%f",&length);
+    int len;
+    scanf("%d",&len);
+    *length = len;
 }
 void input_array(float a[],float length){
     printf("Enter the elements");
     int i;
     for(i=0;i<length;i++){
+        printf("Enter the value for a[%d]",i);
         scanf("%f",&a[i]);
     }
 }
@@ -35,5 +38,11 @@ void bubble_sort(float a[],float length){
 
             }
         }
+    }
+}
+void output(float a[],float length){
+    int i;
+    for(i=0;i<length;i++){
+        printf("%f \n",a[i]);
     }
 }
