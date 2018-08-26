77e8a1672568159f86e6c9c5ba13aa090866954d
shivaraj <sbh69840@gmail.com>
2018-08-26 17:44:35 +0530

    assignment-5-GRADES

--- grades.c
+++ grades.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 void input(float *marks);
-void grades(float marks,char grade[]);
-void output(char grade[]);
+void grades(float marks,char *grade[]);
+void output(char *grade[]);
 void main(){
     float marks;
-    char grade[2];
+    char *grade[3];
     input(&marks);
     grades(marks,grade);
     output(grade);
@@ -15,45 +15,36 @@ void input(float *marks){
     scanf("%f",&mar);
     *marks = mar;
 }
-void grades(float marks,char grade[]){
+void grades(float marks,char *grade[]){
     
     if (marks>95){
-        grade[0] = 'A';
-        grade[1]= '1';
+        *grade = "A1\0";
     }
     else if(marks>85){
-        grade[0]='A';
-        grade[1] = '2';
+        *grade = "A2\0";
     }
     else if(marks>75){
-        grade[0]='B';
-        grade[1]= '1';
+        *grade = "B1\0";
     }
     else if (marks>60){
-        grade[0] = 'B';
-        grade[1] = '2';
+        *grade = "B2\0";
     }
     else if(marks>45){
-        grade[0]= 'C';
-        grade[1]= '1';
+        *grade = "C1\0";
     }
     else if(marks>40){
-        grade[0] = 'C';
-        grade[1] = '2';
+        *grade = "C2\0";
     }
     else if(marks>35){
-        grade[0] = 'D';
-        grade[1] = '1';
+        *grade = "D1\0";
     }
     else if(marks>30){
-        grade[0] = 'D';
-        grade[1] = '2';
+        *grade = "D2\0";
     }
     else{
-        grade[0] = 'E';
-        grade[1] = '1';
+        *grade = "E1\0";
     }
 }
-void output(char grade[]){
-    printf("Your grade is %s",grade);
+void output(char *grade[]){
+    printf("Your grade is %s",*grade);
 }
